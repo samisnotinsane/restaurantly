90d0fbf0796140ca1b3dd748bd4a7cc044cffc23
sameen.islam <sameen.islam@accenture.com>
2019-08-02 18:11:00 +0100

    Patching type error which were breaking build

--- src/app/mock-bar.ts
+++ src/app/mock-bar.ts
@@ -2,9 +2,9 @@ import { Restaurant } from './restaurant';
 
 export const BARS: Restaurant[] = [
     {
-        id: 5,
+        id: '5',
         name: 'Sky Pod Bar',
-        rating: 4.4,
+        rating: '4.4',
         category: 'Bar',
         address: 'Sky Garden, 1 Sky Garden Walk, London EC3M 8AF',
         url: 'http://skygarden.london',
--- src/app/mock-coffee.ts
+++ src/app/mock-coffee.ts
@@ -2,25 +2,25 @@ import { Restaurant } from './restaurant';
 
 export const COFFEES: Restaurant[] = [
     {
-        id: 1,
+        id: '1',
         name: 'Curators Coffee Studio',
-        rating: 4.5,
+        rating: '4.5',
         category: 'Coffee Shop',
         address: '9A Cullum St, London EC3M 7JJ',
         url: 'http://curatorscoffee.com'
     },
     {
-        id: 2,
+        id: '2',
         name: 'Association Coffee',
-        rating: 4.6,
+        rating: '4.6',
         category: 'Coffee Shop',
         address: '10-12 Creechurch Ln, London EC3A 5AY',
         url: 'http://associationcoffee.com',
     },
     {
-        id: 3,
+        id: '3',
         name: 'Fazenda UK',
-        rating: 4.8,
+        rating: '4.8',
         category: 'Coffee Shop',
         address: '9 Harrow Pl, Spitalfields, London E1 7DB',
         url: 'http://fazenda.london',
